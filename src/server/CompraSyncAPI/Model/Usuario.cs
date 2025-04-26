e1df0b7f8cf25523ab4cff73743a3e7769660d5b
JoaoVFranca <joaosouzafranca@outlook.com.br>
2025-04-25 21:51:59 -0300

    Adicionando arquivos do servidor

---
+++ src/server/CompraSyncAPI/Model/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CompraSyncAPI.Model
+{
+    [Table("Usuario")]
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Campo Email obrigatório")]
+        [EmailAddress(ErrorMessage = "Email inválido")]
+        public string Email { get; set; }
+
+        public string Nome { get; set; }
+    }
+}
