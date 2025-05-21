8181c732c2f331178ceded68943669cb7bc36159
MoraisPedroo <pedrointhesky@hotmail.com>
2025-05-21 16:29:05 -0300

    Mudanças

--- src/app/ToDo/src/main.jsx
+++ src/app/ToDo/src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { HashRouter as Router } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+ <Router>
     <App />
-  </BrowserRouter>
+  </Router>,
 );
